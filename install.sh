e7dc4953d32cc4258c390b56777d39fa5bc6ce42
Shubham Kumar Singh <denyshubham@gmail.com>
2023-03-30 08:46:12 -0400

    added wk2 installation

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update -y && sudo apt-get install libc6 -y
+sudo apt install make -y && sudo apt-get install build-essential -y
+sudo apt-get install libssl-dev zlib1g-dev -y
+sudo apt-get install luarocks && luasocket -y
