f008d52ed7a827293e0fae32cba20c7ad736de1c
Shubham Kumar Singh <denyshubham@gmail.com>
2023-04-03 12:23:06 -0400

    added helm

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ sudo apt-get update -y && sudo apt-get install libc6 -y
 sudo apt install make -y && sudo apt-get install build-essential -y
 sudo apt-get install libssl-dev zlib1g-dev -y
 sudo apt-get install luarocks && luasocket -y
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sudo chmod 700 get_helm.sh
+sudo ./get_helm.sh
