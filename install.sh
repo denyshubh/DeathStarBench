a4d9058d2472d9c2945a9e1f750a69fc8c9cfd8e
Shubham Kumar Singh <denyshubham@gmail.com>
2023-04-13 13:16:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update -y && sudo apt-get install libc6 -y
 sudo apt install make -y && sudo apt-get install build-essential -y
 sudo apt-get install libssl-dev zlib1g-dev -y
-sudo apt-get install luarocks && luasocket -y
+sudo apt-get install luarocks
+sudo luarocks install luasocket
